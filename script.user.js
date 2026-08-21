b24de4f379e676040f347b84fbddb31d39852226
GitHub Action <actions@github.com>
2026-08-21 05:37:40 +0000

    自动同步最新域名与油猴脚本

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAVLibrary - 自动更新
 // @namespace    http://tampermonkey.net/
-// @version      3.7.2
+// @version      3.7.3
 // @description  
 // @author       manxyf
 // @match        *://www.f101w.com/cn/*
